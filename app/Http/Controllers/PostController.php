8235a1170491b3f55becb692db5ca72f3f8276fb
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2021-12-13 02:45:05 +0900

    #8-11 データ追加, 抽出, 更新, 削除, Controllerでデータ取得

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,19 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    private $posts = [
-        'Title A',
-        'Title B',
-        'Title C',
-    ];
+    // private $posts = [
+    //     'Title A',
+    //     'Title B',
+    //     'Title C',
+    // ];
 
     public function index()
     {
+        // レコード全件取得
+        // $posts = Post::all();
+
+        // レコードを新規順に並び替え
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        // orderBy('created_at', 'desc')の省略記法
+        $posts = Post::latest()->get();
+
         return view('index')
-            ->with(['posts' => $this->posts]);
+            ->with(['posts' => $posts]);
     }
 
     public function show($id)
